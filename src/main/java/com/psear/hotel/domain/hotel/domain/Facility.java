2795f05e9c42bdd2fc8fc0888cf96a88fb2d9a4c
wooyong99 <wulovesk@naver.com>
2024-04-12 17:20:44 +0900

    feat : Facility 엔티티 구현

---
+++ src/main/java/com/psear/hotel/domain/hotel/domain/Facility.java
@@ -0,0 +1,21 @@
+package com.psear.hotel.domain.hotel.domain;
+
+import com.psear.hotel.domain.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Facility extends BaseEntity {
+    private Boolean parkingLot;
+    private Boolean barbecue;
+    private Boolean wifi;
+}
