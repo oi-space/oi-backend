4723b59df08481bd408df6f58efb15d5898232a0
wooyong99 <wulovesk@naver.com>
2024-04-12 17:18:19 +0900

    refactor : Hotel 엔티티 nullable  어노테이션 추가

--- src/main/java/com/psear/hotel/domain/hotel/domain/Hotel.java
+++ src/main/java/com/psear/hotel/domain/hotel/domain/Hotel.java
@@ -1,6 +1,7 @@
 package com.psear.hotel.domain.hotel.domain;
 
 import com.psear.hotel.domain.model.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import lombok.Getter;
@@ -14,19 +15,33 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class Hotel extends BaseEntity {
+    @Column(unique = true, nullable = false)
     private String name;
+    @Column(nullable = false)
     private String category;
     //    TODO : Enum 구현 예정
+    @Column(nullable = false)
     private String description;
+    @Column(nullable = false)
     private String notice;
+    @Column(nullable = false)
     private String province;
+    @Column(nullable = false)
     private String city;
+    @Column(nullable = false)
     private String district;
+    @Column(nullable = false)
     private String detailedAddress;
+    @Column(nullable = false)
     private double latitude;
+    @Column(nullable = false)
     private double longtitude;
+    @Column(nullable = false)
     private String mainImage;
+    @Column(nullable = false)
     private String businessNumber;
+    @Column(nullable = false)
     private String certUrl;
+    @Column(nullable = false)
     private String visitGuidance;
 }
