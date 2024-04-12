4f9ad03d1b96d921fe59177de1aba3b224b6173d
wooyong99 <wulovesk@naver.com>
2024-04-12 17:09:06 +0900

    feat : Profile 엔티티 임시구현

---
+++ src/main/java/com/psear/hotel/domain/profile/domain/Profile.java
@@ -0,0 +1,26 @@
+package com.psear.hotel.domain.profile.domain;
+
+import com.psear.hotel.domain.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Profile extends BaseEntity {
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String imageUrl;
+    @Column(nullable = false)
+    private String phone;
+}
