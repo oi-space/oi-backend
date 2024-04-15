511897d282d5dca60219e142ceeddb9d299b8c41
wooyong99 <wulovesk@naver.com>
2024-04-15 10:04:20 +0900

    feat : User 엔티티 구현

--- src/main/java/com/psear/hotel/domain/member/domain/User.java
+++ src/main/java/com/psear/hotel/domain/member/domain/User.java
@@ -3,15 +3,16 @@ package com.psear.hotel.domain.member.domain;
 import com.psear.hotel.domain.model.BaseEntity;
 import com.psear.hotel.domain.model.RoleEnum;
 import com.psear.hotel.domain.model.RoleEnumConverter;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
@@ -27,6 +28,10 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     @Convert(converter = RoleEnumConverter.class)
     private RoleEnum role = RoleEnum.ROLE_USER;
+
+    @OneToOne(mappedBy = "user", cascade = {CascadeType.PERSIST,
+            CascadeType.REMOVE}, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Profile profile ;
 
     @Builder
     public User(String email, String password) {
