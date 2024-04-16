76e97ba96726445044e736e96c2ec25b53cc3e83
wooyong99 <wulovesk@naver.com>
2024-04-16 12:55:59 +0900

    feat : 객실 리스트 조회 controller 구현

---
+++ src/main/java/com/pser/hotel/domain/hotel/api/RoomApi.java
@@ -0,0 +1,28 @@
+package com.pser.hotel.domain.hotel.api;
+
+import com.pser.hotel.domain.hotel.dto.RoomResponseDto;
+import com.pser.hotel.global.common.response.ApiResponse;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class RoomApi {
+    // TODO : RoomService DI 예정
+
+    @GetMapping("/rooms")
+    public ResponseEntity<ApiResponse<Page<RoomResponseDto>>> roomList(@PageableDefault Pageable pageable){
+        List<RoomResponseDto> fetch = List.of();
+        long count = 0;
+        Page<RoomResponseDto> result = new PageImpl<>(fetch, pageable,count);
+
+        return ResponseEntity.ok(ApiResponse.success(result));
+    }
+}
