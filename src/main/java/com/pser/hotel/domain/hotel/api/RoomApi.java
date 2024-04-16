755fae8a94c2a37045ac37491eef5d8074887d54
wooyong99 <wulovesk@naver.com>
2024-04-16 12:57:03 +0900

    feat : 객실 단건 조회 controller 구현

--- src/main/java/com/pser/hotel/domain/hotel/api/RoomApi.java
+++ src/main/java/com/pser/hotel/domain/hotel/api/RoomApi.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class RoomApi {
         Page<RoomResponseDto> result = new PageImpl<>(fetch, pageable,count);
 
         return ResponseEntity.ok(ApiResponse.success(result));
+    }
+
+    @GetMapping("/rooms/{roomId}")
+    public ResponseEntity<ApiResponse<RoomResponseDto> roomDetails(@PathVariable Long roomId){
+        RoomResponseDto res = new RoomResponseDto();
+        return ResponseEntity.ok(ApiResponse.success(res));
     }
 }
