370017e0ed6325380b04622746cb374228936e10
wooyong99 <wulovesk@naver.com>
2024-04-17 09:42:10 +0900

    refactor : Response, SearchRequest dto 수정

--- src/main/java/com/pser/hotel/domain/hotel/dto/RoomResponseDto.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/RoomResponseDto.java
@@ -1,4 +1,14 @@
 package com.pser.hotel.domain.hotel.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class RoomResponseDto {
+    private String name;
+
+    public RoomResponseDto(String name) {
+        this.name = name;
+    }
 }
