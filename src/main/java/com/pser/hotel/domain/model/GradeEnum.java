a2b28f67031f12c09199e9247c51863f306d72e4
iwantpark <phy9916@naver.com>
2024-04-17 17:24:13 +0900

    [KSP-89] refactor: GradeEnum 수정, grade를 int형으로 수정

--- src/main/java/com/pser/hotel/domain/model/GradeEnum.java
+++ src/main/java/com/pser/hotel/domain/model/GradeEnum.java
@@ -1,8 +1,33 @@
 package com.pser.hotel.domain.model;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum GradeEnum {
-    EXCELLENT, // 우수
-    GOOD, // 좋음
-    AVERAGE, // 보통
-    POOR // 나쁨
+
+        EXCELLENT(0), // 우수
+        GOOD(1), // 좋음
+        AVERAGE(2), // 보통
+        POOR(3); // 나쁨
+
+        private static final Map<Integer, GradeEnum> valueToGrade =
+                Collections.unmodifiableMap(Stream.of(values())
+                        .collect(Collectors.toMap(GradeEnum::getValue, Function.identity())));
+
+        private final int value;
+
+        GradeEnum(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
+        public static GradeEnum getByValue(int value) {
+            return valueToGrade.get(value);
+        }
 }
