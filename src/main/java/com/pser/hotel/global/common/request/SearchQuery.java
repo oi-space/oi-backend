be52b405f13a0a8ed420f6210fb0875c5b447f8e
amanecopse <amanecopse@gmail.com>
2024-04-17 09:44:27 +0900

    [KSP-80] refactor: SearchQuery 수정
    - 상속을 위해 빌더 어노테이션을 제거

--- src/main/java/com/pser/hotel/global/common/request/SearchQuery.java
+++ src/main/java/com/pser/hotel/global/common/request/SearchQuery.java
@@ -2,14 +2,12 @@ package com.pser.hotel.global.common.request;
 
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class SearchQuery {
     private String keyword;
     private LocalDateTime createdAfter;
