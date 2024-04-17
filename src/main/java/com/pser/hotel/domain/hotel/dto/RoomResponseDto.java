d65bb10b2435fee07e7382e5701ea84014088505
wooyong99 <wulovesk@naver.com>
2024-04-17 10:50:37 +0900

    feat : RoomResponse QueryProjection 임시 구현

--- src/main/java/com/pser/hotel/domain/hotel/dto/RoomResponseDto.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/RoomResponseDto.java
@@ -1,13 +1,17 @@
 package com.pser.hotel.domain.hotel.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 public class RoomResponseDto {
     private String name;
 
+    @QueryProjection
     public RoomResponseDto(String name) {
         this.name = name;
     }
