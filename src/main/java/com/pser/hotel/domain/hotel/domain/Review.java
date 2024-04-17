46dcc9d4092d6cd359f53be8f9857e5f347ed410
iwantpark <phy9916@naver.com>
2024-04-17 16:47:57 +0900

    feat: 리뷰 Entity 연관관계 어노테이션, 기존 BaseEntity속성 인자값 제거

--- src/main/java/com/pser/hotel/domain/hotel/domain/Review.java
+++ src/main/java/com/pser/hotel/domain/hotel/domain/Review.java
@@ -8,8 +8,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @Entity
@@ -19,25 +17,15 @@ public class Review extends BaseEntity {
 
     @Length(min = 10, max = 500) // 상세 내용의 길이를 제한
     private String detail;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST, optional = false)
     @JoinColumn(nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT)) //외래키 제약 조건 추가
     private Reservation reservation;
 
     @Builder
-    public Review(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, String grade, String detail, Reservation reservation) {
-        super(id, createdAt, updatedAt);
+    public Review(String grade, String detail, Reservation reservation) {
         this.grade = grade;
         this.detail = detail;
         this.reservation = reservation;
-    }
-
-    public void setReservation(Reservation reservation){
-        if(this.reservation != null){
-            // null검사와 비슷한 유효성 검사라고 생각하기.
-            // 내가 가지고 있는 Reservertion객체의 List<Review>에서 나(this)를 remove해준다.
-        }
-        this.reservation = reservation;
-        // 넣은 Reservation에 나를 add 추가해준다.
     }
 
 
