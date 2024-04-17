ed8c9b65ee29ba55587bc74d8a7e3b6fc740f66c
iwantpark <phy9916@naver.com>
2024-04-17 17:32:31 +0900

    [KSP-89] feat:  grade 필드를 GradeEnum 타입으로 수정

--- src/main/java/com/pser/hotel/domain/hotel/domain/Review.java
+++ src/main/java/com/pser/hotel/domain/hotel/domain/Review.java
@@ -1,6 +1,8 @@
 package com.pser.hotel.domain.hotel.domain;
 
 import com.pser.hotel.domain.model.BaseEntity;
+import com.pser.hotel.domain.model.GradeEnum;
+import com.pser.hotel.domain.model.GradeEnumConverter;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +15,10 @@ import org.hibernate.validator.constraints.Length;
 @Entity
 @NoArgsConstructor
 public class Review extends BaseEntity {
-    private int grade;
+
+    @Column(nullable = false)
+    @Convert(converter = GradeEnumConverter.class)
+    private GradeEnum grade;
 
     @Length(min = 10, max = 500) // 상세 내용의 길이를 제한
     private String detail;
@@ -22,7 +27,7 @@ public class Review extends BaseEntity {
     private Reservation reservation;
 
     @Builder
-    public Review(int grade, String detail, Reservation reservation) {
+    public Review(GradeEnum grade, String detail, Reservation reservation) {
         this.grade = grade;
         this.detail = detail;
         this.reservation = reservation;
