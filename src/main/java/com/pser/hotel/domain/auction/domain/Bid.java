4cf5b1ab5777570cd47c4314ac7f912185a3002c
wooyong99 <wulovesk@naver.com>
2024-04-18 02:22:58 +0900

    feat : Bid 엔티티 구현

--- src/main/java/com/pser/hotel/domain/auction/domain/Bid.java
+++ src/main/java/com/pser/hotel/domain/auction/domain/Bid.java
@@ -1,21 +1,42 @@
 package com.pser.hotel.domain.auction.domain;
 
+import com.pser.hotel.domain.member.domain.User;
 import com.pser.hotel.domain.model.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {"user_id", "auction_id"}
+                )
+        }
+)
+@Builder
+@AllArgsConstructor
 public class Bid extends BaseEntity {
-    private String userId;
-    private String auctionId;
-    private String price;
-
+    @ManyToOne(cascade = {CascadeType.PERSIST}, fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private User user;
+    @OneToOne(optional = false, fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Auction auction;
+    private int price;
 }
