f410cfece52bf00ec5e3d5120bf3f60f9dee9547
iwantpark <phy9916@naver.com>
2024-04-17 15:30:35 +0900

    feat: 리뷰 Entity 구현 수정

--- src/main/java/com/pser/hotel/domain/hotel/domain/Review.java
+++ src/main/java/com/pser/hotel/domain/hotel/domain/Review.java
@@ -2,13 +2,13 @@ package com.pser.hotel.domain.hotel.domain;
 
 import com.pser.hotel.domain.model.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -16,57 +16,47 @@ import java.time.LocalDateTime;
 @Setter
 @Entity
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 public class Review extends BaseEntity {
     private String grade;
 
     @Length(min = 10, max = 500) // 상세 내용의 길이를 제한
     private String detail;
-
-    private String rentId;
-
-    private String images; // 이미지는 유효성 검사 없이 저장
-
-
-    private String nickname;
-
-    private int rating;
-
-    @CreatedDate
-    @Column(name = "created_time", nullable = false, updatable = false)
-    private LocalDateTime createdTime; // 글 생성 날짜를 저장하는 필드
-
-    @LastModifiedDate
-    @Column(name = "updated_time")
-    private LocalDateTime updatedTime; // 글 수정 날짜를 저장하는 필드
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hotel_id")
-    private Hotel hotel; // 리뷰가 속한 호텔
+    private Reservation reservation;
 
-    @Column(name = "is_deleted")
-    private boolean isDeleted = false; // 삭제 여부를 나타내는 필드
-
-    // 생성자
-    public Review(int rating, String grade, Hotel hotel, String detail, String rentId, String images, String nickname) {
-        this.rating = rating;
+    @Builder
+    public Review(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, String grade, String detail, Reservation reservation) {
+        super(id, createdAt, updatedAt);
         this.grade = grade;
         this.detail = detail;
-        this.rentId = rentId;
-        this.images = images;
-        this.nickname = nickname;
-        this.hotel = hotel;
-        // createdTime과 updatedTime은 각각 @CreatedDate, @LastModifiedDate 어노테이션에 의해 자동 설정 됨.
+        this.reservation = reservation;
     }
 
-    public void updateReview(int rating, String detail, String images) {
-        this.rating = rating;
-        this.detail = detail;
-        this.images = images;
-        // updatedTime은 @LastModifiedDate 어노테이션에 의해 자동 설정 됨
+    public void setReservation(Reservation reservation){
+        if(this.reservation != null){
+            // null검사와 비슷한 유효성 검사라고 생각하기.
+            // 내가 가지고 있는 Reservertion객체의 List<Review>에서 나(this)를 remove해준다.
+        }
+        this.reservation = reservation;
+        // 넣은 Reservation에 나를 add 추가해준다.
     }
 
-    public void deleteReview() {
-        this.isDeleted = true;
-    }
+    //    private String images; // 이미지는 유효성 검사 없이 저장
+
+
+//    public Review(int rating, String grade, Hotel hotel, String detail, String rentId, String images, String nickname) {
+//        this.grade = grade;
+//        this.detail = detail;
+//        this.rentId = rentId;
+//        this.images = images;
+//        this.hotel = hotel;
+//        // createdTime과 updatedTime은 각각 @CreatedDate, @LastModifiedDate 어노테이션에 의해 자동 설정 됨.
+//    }
+
+//    public void updateReview(int rating, String detail, String images) {
+//        this.detail = detail;
+//        this.images = images;
+//        // updatedTime은 @LastModifiedDate 어노테이션에 의해 자동 설정 됨
+//    }
 }
