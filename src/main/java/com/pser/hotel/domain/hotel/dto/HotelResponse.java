08c06bb99309ce6dbf03b6a3a6219354bf969036
uiheonn <ky7662@naver.com>
2024-04-19 11:11:00 +0900

    refactor : HotelResponse 수정

--- src/main/java/com/pser/hotel/domain/hotel/dto/HotelResponse.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/HotelResponse.java
@@ -1,13 +1,48 @@
 package com.pser.hotel.domain.hotel.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class HotelResponse {
-    private String response;
-    public HotelResponse(String response){
-        this.response = response;
-    }
+    private Long id;
+
+    private Long userId;
+
+    private String name;
+
+    private String category;
+
+    private String description;
+
+    private String notice;
+
+    private String province;
+
+    private String city;
+
+    private String district;
+
+    private String detailedAddress;
+
+    private Double latitude;
+
+    private Double longtitude;
+
+    private String mainImage;
+
+    private String businessNumber;
+
+    private String certUrl;
+
+    private String visitGuidance;
+
+    private Boolean parkingLot;
+
+    private Boolean wifi;
+
+    private Boolean barbecue;
 }
