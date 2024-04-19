c91dc2bfb2873e4ee2899b50ea6d1a0d6df21972
wooyong99 <wulovesk@naver.com>
2024-04-19 14:54:36 +0900

    feat : 의존되는 HotelDao 임시 구현 및 의존 메서드 구현

--- src/main/java/com/pser/hotel/domain/hotel/dao/RoomDao.java
+++ src/main/java/com/pser/hotel/domain/hotel/dao/RoomDao.java
@@ -3,6 +3,7 @@ package com.pser.hotel.domain.hotel.dao;
 import com.pser.hotel.domain.hotel.domain.Room;
 import com.pser.hotel.domain.hotel.dto.RoomResponseDto;
 import com.pser.hotel.domain.hotel.dto.RoomSearchRequest;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RoomDao extends JpaRepository<Room, Long>, RoomDaoCustom {
     @Override
     Page<RoomResponseDto> search(RoomSearchRequest request, Pageable pageable);
+
+    Optional<Room> findByIdAndHotelId(Long roomId, Long id);
 }
