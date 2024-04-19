5193babf564c6363dd0690411ce62f6b29da2944
uiheonn <ky7662@naver.com>
2024-04-19 11:12:04 +0900

    refactor : Long 타입, ResponseEntity 수정

--- src/main/java/com/pser/hotel/domain/hotel/api/HotelApi.java
+++ src/main/java/com/pser/hotel/domain/hotel/api/HotelApi.java
@@ -4,13 +4,13 @@ import com.pser.hotel.domain.hotel.dto.HotelCreateRequest;
 import com.pser.hotel.domain.hotel.dto.HotelResponse;
 import com.pser.hotel.domain.hotel.dto.HotelSearchRequest;
 import com.pser.hotel.global.common.response.ApiResponse;
+import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,8 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HotelApi {
 
     @PostMapping // 숙소 등록 api
-    public ResponseEntity<ApiResponse<Void>> saveHotel(@RequestBody HotelCreateRequest hotelCreateRequest, @RequestHeader("user-id") Long userId) {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<ApiResponse<Void>> saveHotel(@RequestBody HotelCreateRequest hotelCreateRequest, @RequestHeader("user-id") long userId) {
+        return ResponseEntity.created(URI.create("/hotels/" + 1)).build();
     }
 
     @GetMapping // 숙소 전체 조회
@@ -51,17 +51,20 @@ public class HotelApi {
 
     @GetMapping("/{hotelId}") // 특정 숙소 조회
     public ResponseEntity<ApiResponse<HotelResponse>> getHotel(@PathVariable Long hotelId){
-        HotelResponse hotelRequest = new HotelResponse("first controller");
-        return ResponseEntity.ok(ApiResponse.success(hotelRequest));
+        HotelResponse hotelResponse = HotelResponse.builder()
+            .id(hotelId)
+            .name("쏠비치 양양")
+            .build();
+        return ResponseEntity.ok(ApiResponse.success(hotelResponse));
     }
 
     @PatchMapping("/{hotelId}") // 숙소 수정
-    public ResponseEntity<ApiResponse<Void>> updateHotel(HotelCreateRequest hotelCreateRequest, @PathVariable Long hotelId, @RequestHeader("user-id") Long userId){
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    public ResponseEntity<ApiResponse<Void>> updateHotel(HotelCreateRequest hotelCreateRequest, @PathVariable Long hotelId, @RequestHeader("user-id") long userId){
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{hotelId}") // 숙소 삭제
-    public ResponseEntity<ApiResponse<Void>> deleteHotel(HotelCreateRequest hotelCreateRequest, @PathVariable Long hotelId, @RequestHeader("user-id") Long userId){
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    public ResponseEntity<ApiResponse<Void>> deleteHotel(HotelCreateRequest hotelCreateRequest, @PathVariable Long hotelId, @RequestHeader("user-id") long userId){
+        return ResponseEntity.noContent().build();
     }
 }
