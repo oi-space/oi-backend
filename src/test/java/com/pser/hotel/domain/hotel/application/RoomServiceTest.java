3801dd87cdd5759caa0f781886f158b21a7a655c
wooyong99 <wulovesk@naver.com>
2024-04-19 14:31:51 +0900

    test : RoomService findRoomList 테스트 코드 작성

--- src/test/java/com/pser/hotel/domain/hotel/application/RoomServiceTest.java
+++ src/test/java/com/pser/hotel/domain/hotel/application/RoomServiceTest.java
@@ -2,21 +2,23 @@ package com.pser.hotel.domain.hotel.application;
 
 import static com.pser.hotel.domain.hotel.util.Utils.createHotel;
 import static com.pser.hotel.domain.hotel.util.Utils.createRoom;
+import static com.pser.hotel.domain.hotel.util.Utils.createRooms;
 import static com.pser.hotel.domain.hotel.util.Utils.createUser;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
 
 import com.pser.hotel.domain.hotel.dao.HotelDao;
 import com.pser.hotel.domain.hotel.dao.RoomDao;
 import com.pser.hotel.domain.hotel.domain.Hotel;
 import com.pser.hotel.domain.hotel.domain.Room;
 import com.pser.hotel.domain.hotel.dto.RoomRequestDto;
+import com.pser.hotel.domain.hotel.dto.RoomResponseDto;
 import com.pser.hotel.domain.member.domain.User;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,6 +27,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("객실 Service 테스트")
@@ -64,6 +70,23 @@ class RoomServiceTest {
         then(roomDao).should().save(any());
     }
 
+    @Test
+    @DisplayName("findRoomList 테스트")
+    public void findRoomList() {
+        // Given
+        List<Room> roomList = createRooms(hotel, 10);
+        Pageable pageable = createPageable(0, 10);
+        Page<Room> roomPage = new PageImpl<>(roomList, pageable, 10);
+
+        given(roomDao.findAll(pageable)).willReturn(roomPage);
+
+        // When
+        Page<RoomResponseDto> result = roomService.findRoomList(pageable);
+
+        // Then
+        Assertions.assertThat(result).isEqualTo(roomPage.map(e -> e.toDto()));
+    }
+
     private RoomRequestDto createRoomRequestDto() {
         return new RoomRequestDto(
                 1L,
@@ -74,5 +97,9 @@ class RoomServiceTest {
                 rnd.nextBoolean(), rnd.nextBoolean(), rnd.nextBoolean(), rnd.nextBoolean(), rnd.nextBoolean(),
                 rnd.nextBoolean()
         );
+    }
+
+    private Pageable createPageable(int pageNumber, int pageSize) {
+        return PageRequest.of(pageNumber, pageSize);
     }
 }
