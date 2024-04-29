6d41203a7999bbbd3ba12e0c9f2be82aeed37dbb
iwantpark <phy9916@naver.com>
2024-04-29 09:43:02 +0900

    [KSP-112] feat: Request, Response Dto 코드 생성

--- src/main/java/com/pser/hotel/domain/hotel/dto/ReviewResponseDto.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/ReviewResponseDto.java
@@ -1,4 +1,26 @@
 package com.pser.hotel.domain.hotel.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.pser.hotel.domain.model.GradeEnum;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ReviewResponseDto {
+
+    private Long id;
+
+    private GradeEnum grade;
+
+    private String detail;
+
+    private Long reservationId; // 연결된 예약 ID
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime createdAt; // 생성 시간
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime updatedAt; // 수정 시간
 }
