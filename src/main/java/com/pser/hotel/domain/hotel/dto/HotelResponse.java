783a2c82183266ccb97b032650850a0c1fba9053
uiheonn <ky7662@naver.com>
2024-05-02 09:42:41 +0900

    refactor : hotel response 수정

--- src/main/java/com/pser/hotel/domain/hotel/dto/HotelResponse.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/HotelResponse.java
@@ -1,12 +1,16 @@
 package com.pser.hotel.domain.hotel.dto;
 
+import com.pser.hotel.domain.hotel.domain.Hotel;
+import com.pser.hotel.domain.hotel.domain.HotelCategoryEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class HotelResponse {
     private Long id;
 
@@ -14,7 +18,7 @@ public class HotelResponse {
 
     private String name;
 
-    private String category;
+    private HotelCategoryEnum category;
 
     private String description;
 
@@ -45,4 +49,26 @@ public class HotelResponse {
     private Boolean wifi;
 
     private Boolean barbecue;
+
+    public HotelResponse(Hotel hotel) {
+        this.id = hotel.getId();
+        this.userId = hotel.getUser().getId();
+        this.name = hotel.getName();
+        this.category = hotel.getCategory();
+        this.description = hotel.getDescription();
+        this.notice = hotel.getNotice();
+        this.province = hotel.getProvince();
+        this.city = hotel.getCity();
+        this.district = hotel.getDistrict();
+        this.detailedAddress = hotel.getDetailedAddress();
+        this.latitude = hotel.getLatitude();
+        this.longtitude = hotel.getLongtitude();
+        this.mainImage = hotel.getMainImage();
+        this.businessNumber = hotel.getBusinessNumber();
+        this.certUrl = hotel.getCertUrl();
+        this.visitGuidance = hotel.getVisitGuidance();
+        this.parkingLot = hotel.getFacility().getParkingLot();
+        this.wifi = hotel.getFacility().getWifi();
+        this.barbecue = hotel.getFacility().getBarbecue();
+    }
 }
