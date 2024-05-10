d5913b12123d7ec80929f7f9a8e3135c538dd592
uiheonn <ky7662@naver.com>
2024-05-10 10:11:56 +0900

    refactor : update request dto 수정

--- src/main/java/com/pser/hotel/domain/hotel/dto/HotelUpdateRequest.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/HotelUpdateRequest.java
@@ -1,7 +1,6 @@
 package com.pser.hotel.domain.hotel.dto;
 
 import com.pser.hotel.domain.hotel.domain.HotelCategoryEnum;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,64 +10,63 @@ import org.hibernate.validator.constraints.URL;
 @Builder
 @AllArgsConstructor
 public class HotelUpdateRequest {
-    @NotBlank
     private String name;
-    @NotBlank
+
     private HotelCategoryEnum category;
-    @NotBlank
+
     private String description;
-    @NotBlank
+
     private String notice;
-    @NotBlank
+
     private String province;
-    @NotBlank
+
     private String city;
-    @NotBlank
+
     private String district;
-    @NotBlank
+
     private String detailedAddress;
-    @NotBlank
+
     private Double latitude;
-    @NotBlank
+
     private Double longtitude;
-    @NotBlank
+
     @URL(message = "URL 형식이 유효하지 않습니다.")
     private String mainImage;
-    @NotBlank
+
     private String businessNumber;
-    @NotBlank
+
     @URL(message = "URL 형식이 유효하지 않습니다.")
     private String certUrl;
-    @NotBlank
+
     private String visitGuidance;
-    @NotBlank
+
     private Boolean parkingLot;
-    @NotBlank
+
     private Boolean wifi;
-    @NotBlank
+
     private Boolean barbecue;
-    @NotBlank
+
     private Boolean sauna;
-    @NotBlank
+
     private Boolean swimmingPool;
-    @NotBlank
+
     private Boolean restaurant;
-    @NotBlank
+
     private Boolean roofTop;
-    @NotBlank
+
     private Boolean fitness;
-    @NotBlank
+
     private Boolean dryer;
-    @NotBlank
+
     private Boolean breakfast;
-    @NotBlank
+
     private Boolean smokingArea;
-    @NotBlank
+
     private Boolean allTimeDesk;
-    @NotBlank
+
     private Boolean luggageStorage;
-    @NotBlank
+
     private Boolean snackBar;
-    @NotBlank
+
     private Boolean petFriendly;
 }
