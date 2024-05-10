f2cf5436da7e017b02fb697805ac3b87ea91b19b
uiheonn <ky7662@naver.com>
2024-05-10 11:09:51 +0900

    refactor : dao findByName 추가

--- src/main/java/com/pser/hotel/domain/hotel/dao/RoomDao.java
+++ src/main/java/com/pser/hotel/domain/hotel/dao/RoomDao.java
@@ -7,10 +7,13 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface RoomDao extends JpaRepository<Room, Long>, RoomDaoCustom {
     @Override
     Page<RoomResponse> search(RoomSearchRequest request, Pageable pageable);
 
     Optional<Room> findByIdAndHotelId(Long roomId, Long id);
+
+    Optional<Room> findByName(@Param("name") String name);
 }
