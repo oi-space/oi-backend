4a0d3df8da51f69fa91fbfe85a42bf2fcfa7015a
iwantpark <phy9916@naver.com>
2024-05-12 22:05:18 +0900

    [KSP-173] feat: ReviewUpdateRequest 클래스 생성

---
+++ src/main/java/com/pser/hotel/domain/hotel/dto/ReviewUpdateRequest.java
@@ -0,0 +1,20 @@
+package com.pser.hotel.domain.hotel.dto;
+
+import com.pser.hotel.domain.model.GradeEnum;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ReviewUpdateRequest {
+
+    private GradeEnum grade;
+
+    @Size(min = 10, max = 500, message = "리뷰 내용은 최소 10자 이상, 최대 500자 이하로 작성해주세요.")
+    private String detail;
+
+    private List<String> imageUrls;
+
+    // 위에 grade, detail, imageUrls는 수정시 필수가 아닐 수 있음을 고려함.
+}
