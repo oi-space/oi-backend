4a8c26dea0699acb17b6f9539fa12caa2f0ea309
iwantpark <phy9916@naver.com>
2024-05-12 22:52:30 +0900

    [KSP-173] feat: ReviewSearchRequest 클래스 생성

---
+++ src/main/java/com/pser/hotel/domain/hotel/dto/ReviewSearchRequest.java
@@ -0,0 +1,29 @@
+package com.pser.hotel.domain.hotel.dto;
+
+import com.pser.hotel.domain.model.GradeEnum;
+import com.pser.hotel.global.common.request.SearchQuery;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ReviewSearchRequest extends SearchQuery {
+
+    private GradeEnum grade;
+    private String detail;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    @Builder
+    public ReviewSearchRequest(String keyword, LocalDateTime createdAfter, LocalDateTime createdBefore,
+                               LocalDateTime updatedAfter, LocalDateTime updatedBefore, GradeEnum grade, String detail,
+                               LocalDateTime startDate, LocalDateTime endDate) {
+        super(keyword, createdAfter, createdBefore, updatedAfter, updatedBefore);
+        this.grade = grade;
+        this.detail = detail;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
