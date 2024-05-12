7a549a1efde71ba30cd819642344a127e8ec107e
iwantpark <phy9916@naver.com>
2024-05-12 21:56:04 +0900

    [KSP-173] feat: 기존 dto 제거 후 eviewCreateRequest 클래스 생성

---
+++ src/main/java/com/pser/hotel/domain/hotel/dto/ReviewCreateRequest.java
@@ -0,0 +1,25 @@
+package com.pser.hotel.domain.hotel.dto;
+
+import com.pser.hotel.domain.model.GradeEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ReviewCreateRequest {
+
+    @NotNull
+    private GradeEnum grade;
+
+    @NotBlank
+    @Size(min = 10, max = 500)
+    private String detail;
+
+    @NotNull
+    private Long reservationId;
+
+    private List<String> imageUrls; // 사진 등록 기능을 위한 필드 추가
+}
