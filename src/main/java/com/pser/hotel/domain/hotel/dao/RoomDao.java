53ebb5e7d8bcc54698b39d31ed1fe2abb5ea4923
givemea1 <givemea4702@gmail.com>
2024-05-20 10:58:57 +0900

    refactor : room dao 수정

--- src/main/java/com/pser/hotel/domain/hotel/dao/RoomDao.java
+++ src/main/java/com/pser/hotel/domain/hotel/dao/RoomDao.java
@@ -1,19 +1,18 @@
 package com.pser.hotel.domain.hotel.dao;
 
 import com.pser.hotel.domain.hotel.domain.Room;
-import com.pser.hotel.domain.hotel.dto.RoomResponse;
+import com.pser.hotel.domain.hotel.dto.RoomResponseDto;
 import com.pser.hotel.domain.hotel.dto.RoomSearchRequest;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface RoomDao extends JpaRepository<Room, Long>, RoomDaoCustom {
     @Override
-    Page<RoomResponse> search(RoomSearchRequest request, Pageable pageable);
-
-    Optional<Room> findByIdAndHotelId(Long roomId, Long id);
+    Page<RoomResponseDto> search(RoomSearchRequest request, Pageable pageable);
 
     Optional<Room> findByName(@Param("name") String name);
 }
