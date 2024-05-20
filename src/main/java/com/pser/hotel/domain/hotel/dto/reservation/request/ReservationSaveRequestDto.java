95f7d1e67e92389bdf3c725e6bce63f54a1d4ce1
givemea1 <givemea4702@gmail.com>
2024-05-20 11:01:31 +0900

    refactor : 예약 save dto 수정

--- src/main/java/com/pser/hotel/domain/hotel/dto/reservation/request/ReservationSaveRequestDto.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/reservation/request/ReservationSaveRequestDto.java
@@ -1,6 +1,9 @@
 package com.pser.hotel.domain.hotel.dto.reservation.request;
 
 import com.pser.hotel.domain.hotel.domain.ReservationEnum;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,14 +17,27 @@ import java.time.LocalDate;
 @Builder
 public class ReservationSaveRequestDto {
     // 회원 탐색을 위한 user email
+    @NotBlank
     private String userEmail;
     // 객실 예약을 위한 room name
+    @NotBlank
     private String roomName;
+    @NotBlank
+    @Min(0)
     private Integer price;
+    @NotBlank
     private LocalDate startAt;
+    @NotBlank
     private LocalDate endAt;
+    @NotBlank
+    @Min(0)
     private Integer reservationCapacity;
+    @NotBlank
+    @Min(0)
     private Integer adultCapacity;
+    @NotBlank
+    @Min(0)
     private Integer childCapacity;
+    @NotBlank
     private ReservationEnum status;
 }
