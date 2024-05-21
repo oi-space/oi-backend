f33d28b4a008b43376dd54985a462dc528a0c356
Jung ui heon <ky7662@naver.com>
2024-05-21 10:03:11 +0900

    refactor : timesale response 수정

--- src/main/java/com/pser/hotel/domain/hotel/dto/TimesaleHotelResponse.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/TimesaleHotelResponse.java
@@ -1,11 +1,10 @@
 package com.pser.hotel.domain.hotel.dto;
 
 import com.pser.hotel.domain.hotel.domain.HotelCategoryEnum;
-import lombok.Builder;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 
 @Data
-@Builder
 public class TimesaleHotelResponse {
     private Long id;
 
@@ -67,7 +66,50 @@ public class TimesaleHotelResponse {
 
     private Boolean petFriendly;
 
+    private int salePrice;
+
     private int previousPrice;
 
-    private int salePrice;
+    @QueryProjection
+    public TimesaleHotelResponse(Long id, String name, HotelCategoryEnum category, String description,
+                         String notice, String province, String city, String district, String detailedAddress,
+                         Double latitude,
+                         Double longtitude, String mainImage, String businessNumber, String certUrl,
+                         String visitGuidance,
+                         Boolean parkingLot, Boolean wifi, Boolean barbecue, Boolean sauna, Boolean swimmingPool,
+                         Boolean restaurant,
+                         Boolean roofTop, Boolean fitness, Boolean dryer, Boolean breakfast, Boolean smokingArea,
+                         Boolean allTimeDesk,
+                         Boolean luggageStorage, Boolean snackBar, Boolean petFriendly) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.notice = notice;
+        this.province = province;
+        this.city = city;
+        this.district = district;
+        this.detailedAddress = detailedAddress;
+        this.latitude = latitude;
+        this.longtitude = longtitude;
+        this.mainImage = mainImage;
+        this.businessNumber = businessNumber;
+        this.certUrl = certUrl;
+        this.visitGuidance = visitGuidance;
+        this.parkingLot = parkingLot;
+        this.wifi = wifi;
+        this.barbecue = barbecue;
+        this.sauna = sauna;
+        this.swimmingPool = swimmingPool;
+        this.restaurant = restaurant;
+        this.roofTop = roofTop;
+        this.fitness = fitness;
+        this.dryer = dryer;
+        this.breakfast = breakfast;
+        this.smokingArea = smokingArea;
+        this.allTimeDesk = allTimeDesk;
+        this.luggageStorage = luggageStorage;
+        this.snackBar = snackBar;
+        this.petFriendly = petFriendly;
+    }
 }
