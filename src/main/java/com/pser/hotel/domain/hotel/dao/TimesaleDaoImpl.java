aedd9b24176e27d875f5e09bff37a54d75ede450
Jung ui heon <ky7662@naver.com>
2024-05-21 10:02:44 +0900

    refactor : timesale impl 메서드 추가

--- src/main/java/com/pser/hotel/domain/hotel/dao/TimesaleDaoImpl.java
+++ src/main/java/com/pser/hotel/domain/hotel/dao/TimesaleDaoImpl.java
@@ -4,15 +4,33 @@ import com.pser.hotel.domain.hotel.domain.Hotel;
 import com.pser.hotel.domain.hotel.domain.QHotel;
 import com.pser.hotel.domain.hotel.domain.QRoom;
 import com.pser.hotel.domain.hotel.domain.QTimeSale;
+import com.pser.hotel.domain.hotel.domain.Room;
+import com.pser.hotel.domain.hotel.domain.TimeSale;
+import com.pser.hotel.domain.hotel.dto.TimesaleCreateRequest;
+import com.pser.hotel.domain.hotel.dto.TimesaleHotelResponse;
+import com.pser.hotel.domain.hotel.dto.TimesaleMapper;
+import com.pser.hotel.domain.hotel.dto.TimesaleRoomResponse;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class TimesaleDaoImpl implements TimesaleCustom {
     private final JPAQueryFactory queryFactory;
+    private final TimesaleMapper timesaleMapper;
+    private final TimesaleDao timesaleDao;
 
     @Override
     public Optional<Hotel> findHotelByRoomId(Long roomId) {
@@ -42,5 +60,73 @@ public class TimesaleDaoImpl implements TimesaleCustom {
                 .fetchOne();
 
         return Optional.ofNullable(result);
+    }
+
+    @Override
+    public Slice<TimesaleHotelResponse> findNowTimesaleHotel(Pageable pageable) {
+        QRoom qRoom = QRoom.room;
+        QHotel qHotel = QHotel.hotel;
+        QTimeSale qTimeSale = QTimeSale.timeSale;
+        LocalDateTime now = LocalDateTime.now();
+
+        List<Tuple> timesaleData = queryFactory
+                .select(qTimeSale, qRoom, qHotel)
+                .from(qTimeSale)
+                .join(qTimeSale.room, qRoom)
+                .join(qRoom.hotel, qHotel)
+                .where(qTimeSale.startAt.before(now).and(qTimeSale.endAt.after(now)))
+                .fetch();
+
+        List<TimesaleHotelResponse> timesaleHotelResponses = timesaleData.stream()
+                .map(tuple -> {
+                    TimeSale timeSale = tuple.get(qTimeSale);
+                    Room room = tuple.get(qRoom);
+                    Hotel hotel = tuple.get(qHotel);
+
+                    TimesaleRoomResponse roomResponse = timesaleMapper.changeToTimesaleRoomResponse(room,
+                            Objects.requireNonNull(timeSale).getPrice());
+
+                    return timesaleMapper.changeToTimeHotelResponse(
+                            hotel,
+                            roomResponse.getPreviousPrice(),
+                            roomResponse.getSalePrice()
+                    );
+                })
+                .toList();
+
+        Map<String, TimesaleHotelResponse> hotelResponseMap = timesaleHotelResponses.stream()
+                .collect(Collectors.toMap(
+                        TimesaleHotelResponse::getName,
+                        response -> response,
+                        (existing, replacement) -> existing.getSalePrice() < replacement.getSalePrice() ? existing
+                                : replacement
+                ));
+
+        List<TimesaleHotelResponse> filteredHotelResponses = new ArrayList<>(hotelResponseMap.values());
+
+        int start = (int) pageable.getOffset();
+        int end = Math.min(start + pageable.getPageSize(), filteredHotelResponses.size());
+        List<TimesaleHotelResponse> subList = filteredHotelResponses.subList(start, end);
+
+        boolean hasNext = end < filteredHotelResponses.size();
+
+        return new SliceImpl<>(subList, pageable, hasNext);
+    }
+
+    @Override
+    public boolean checkTimesaleTimeIsValid(TimesaleCreateRequest dto) {
+        List<TimeSale> timeSaleList = timesaleDao.findByRoomId(dto.getRoomId());
+        if (timeSaleList.isEmpty()) { // 같은 객실 타임특가가 없다면 체킹할 필요 X
+            return true;
+        }
+        for (TimeSale timeSale : timeSaleList) {
+            if (timeSale.getStartAt().isAfter(dto.getEndAt()) || timeSale.getEndAt().isBefore(dto.getStartAt())) {
+                continue;
+            } else {
+                // 겹치는 시간이 존재하면 return false;
+                return false;
+            }
+        }
+        return true;
     }
 }
