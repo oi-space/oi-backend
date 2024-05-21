24adf8a2320cc8fdd47f4a3e7c9021f18453070f
Jung ui heon <ky7662@naver.com>
2024-05-21 10:02:00 +0900

    refactor : timesale dao custom 수정

--- src/main/java/com/pser/hotel/domain/hotel/dao/TimesaleCustom.java
+++ src/main/java/com/pser/hotel/domain/hotel/dao/TimesaleCustom.java
@@ -1,10 +1,18 @@
 package com.pser.hotel.domain.hotel.dao;
 
 import com.pser.hotel.domain.hotel.domain.Hotel;
+import com.pser.hotel.domain.hotel.dto.TimesaleCreateRequest;
+import com.pser.hotel.domain.hotel.dto.TimesaleHotelResponse;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface TimesaleCustom {
     Optional<Hotel> findHotelByRoomId(Long roomId);
 
     Optional<Hotel> findHotelByTimesaleId(Long timesaleId);
+
+    Slice<TimesaleHotelResponse> findNowTimesaleHotel(Pageable pageable);
+
+    boolean checkTimesaleTimeIsValid(TimesaleCreateRequest timesaleCreateRequest);
 }
