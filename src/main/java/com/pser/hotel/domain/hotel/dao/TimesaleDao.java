3b566e45b26d359f1133d9c844b2030c1d9087f2
Jung ui heon <ky7662@naver.com>
2024-05-21 10:02:20 +0900

    refactor : timesale dao 메서드 추가

--- src/main/java/com/pser/hotel/domain/hotel/dao/TimesaleDao.java
+++ src/main/java/com/pser/hotel/domain/hotel/dao/TimesaleDao.java
@@ -1,7 +1,9 @@
 package com.pser.hotel.domain.hotel.dao;
 
 import com.pser.hotel.domain.hotel.domain.TimeSale;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TimesaleDao extends JpaRepository<TimeSale, Long>, TimesaleCustom {
+    List<TimeSale> findByRoomId(Long roomId);
 }
