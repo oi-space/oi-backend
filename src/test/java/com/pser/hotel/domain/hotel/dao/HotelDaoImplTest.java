44390ba968d900f52e9ec9d78d3b6d2421f7188d
Jung ui heon <ky7662@naver.com>
2024-05-23 12:39:55 +0900

    test : hotel impl mapperConfig 설정 추가

--- src/test/java/com/pser/hotel/domain/hotel/dao/HotelDaoImplTest.java
+++ src/test/java/com/pser/hotel/domain/hotel/dao/HotelDaoImplTest.java
@@ -1,14 +1,23 @@
 package com.pser.hotel.domain.hotel.dao;
 
+import com.pser.hotel.domain.hotel.config.MapperConfig;
 import com.pser.hotel.domain.hotel.domain.Facility;
 import com.pser.hotel.domain.hotel.domain.Hotel;
 import com.pser.hotel.domain.hotel.domain.HotelCategoryEnum;
+import com.pser.hotel.domain.hotel.domain.Reservation;
+import com.pser.hotel.domain.hotel.domain.Room;
+import com.pser.hotel.domain.hotel.dto.HotelMapper;
+import com.pser.hotel.domain.hotel.dto.HotelMapperImpl;
 import com.pser.hotel.domain.hotel.dto.HotelResponse;
 import com.pser.hotel.domain.hotel.dto.HotelSearchRequest;
+import com.pser.hotel.domain.hotel.dto.TimesaleMapper;
+import com.pser.hotel.domain.hotel.dto.TimesaleMapperImpl;
 import com.pser.hotel.domain.hotel.util.Utils;
 import com.pser.hotel.domain.member.domain.User;
 import com.pser.hotel.global.config.QueryDslConfig;
 import jakarta.persistence.EntityManager;
+import java.time.LocalDate;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +25,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,7 +34,7 @@ import org.springframework.data.domain.Slice;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import({QueryDslConfig.class})
+@Import({QueryDslConfig.class, MapperConfig.class})
 public class HotelDaoImplTest {
     @Autowired
     HotelDao hotelDao;
@@ -32,10 +43,14 @@ public class HotelDaoImplTest {
     @Autowired
     UserDao userDao;
     @Autowired
+    RoomDao roomDao;
+    @Autowired
     EntityManager entityManager;
     User user;
     Hotel hotel;
     Facility facility;
+    Room room;
+    Reservation reservation;
     PageRequest pageRequest;
     HotelSearchRequest hotelSearchRequest;
 
@@ -44,9 +59,13 @@ public class HotelDaoImplTest {
         pageRequest = PageRequest.of(0, 10);
         user = Utils.createUser();
         hotel = Utils.createHotel(user);
+        room = Utils.createRoom(hotel);
+        reservation = Utils.createReservation(user, room);
         facility = Utils.createFacility(hotel);
+
         userDao.save(user);
         hotelDao.save(hotel);
+        roomDao.save(room);
         facilityDao.save(facility);
     }
 
@@ -123,6 +142,59 @@ public class HotelDaoImplTest {
         return HotelSearchRequest.builder()
                 .detailedAddress(detailedAddress)
                 .build();
+    }
+
+    @Test
+    @DisplayName("people 검색 테스트")
+    public void peopleTest() {
+        Pageable pageable = createPageable();
+        hotelSearchRequest = createSearchRequestByPeople(room.getMaxCapacity());
+        Slice<HotelResponse> hotelResponse = hotelDao.search(hotelSearchRequest, pageable);
+        Assertions.assertThat(hotelResponse.getContent()).isNotEmpty();
+    }
+
+    private HotelSearchRequest createSearchRequestByPeople(int maxCapacity) {
+        return HotelSearchRequest.builder()
+                .people(maxCapacity - 1)
+                .build();
+    }
+
+    @Test
+    @DisplayName("startAt, endAt 검색 테스트")
+    public void dateTest() {
+        Pageable pageable = createPageable();
+        hotelSearchRequest = createSearchRequestByDate();
+        Slice<HotelResponse> hotelResponse = hotelDao.search(hotelSearchRequest, pageable);
+        Assertions.assertThat(hotelResponse.getContent()).isNotEmpty();
+    }
+
+    private HotelSearchRequest createSearchRequestByDate() {
+        return HotelSearchRequest.builder()
+                .searchStartAt(LocalDate.of(2024,4,18))
+                .searchEndAt(LocalDate.of(2024,4,19))
+                .build();
+    }
+
+    @Test
+    @DisplayName("숙소 평점 테스트")
+    public void gradeTest() {
+        double grade = hotelDao.getHotelGrade(hotel.getId());
+        Assertions.assertThat(grade).isGreaterThan(-1);
+    }
+
+    @Test
+    @DisplayName("숙소 전체 조회 테스트")
+    public void fndAllTest() {
+        Pageable pageable = createPageable();
+        Slice<HotelResponse> hotelResponse = hotelDao.findAllWithGradeAndPrice(pageable);
+        Assertions.assertThat(hotelResponse).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("특정 숙소 조회 테스트")
+    public void findOneHotel() {
+        HotelResponse hotelResponse = hotelDao.findHotel(hotel.getId());
+        Assertions.assertThat(hotelResponse).isNotNull();
     }
 
     private Pageable createPageable() {
