27d7e47b0ea1bae95152cbb4f07029393a80742f
iwantpark <phy9916@naver.com>
2024-05-23 14:15:20 +0900

    [KSP-173] feat: ReviewDaoCustom 기능추가

--- src/main/java/com/pser/hotel/domain/hotel/dao/ReviewDaoCustom.java
+++ src/main/java/com/pser/hotel/domain/hotel/dao/ReviewDaoCustom.java
@@ -1,4 +1,11 @@
 package com.pser.hotel.domain.hotel.dao;
 
+import com.pser.hotel.domain.hotel.domain.Review;
+import com.pser.hotel.domain.hotel.dto.ReviewSearchRequest;
+import com.pser.hotel.global.common.request.SearchQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ReviewDaoCustom {
+    Page<Review> search(ReviewSearchRequest request, SearchQuery searchQuery, Pageable pageable);
 }
