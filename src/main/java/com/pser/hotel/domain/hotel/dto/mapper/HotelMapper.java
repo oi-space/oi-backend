b2da7626bbf3d8c08cd10cd300ea10137af28d55
amanecopse <amanecopse@gmail.com>
2024-05-30 14:14:59 +0900

    [KSP-252] feat: DTO 관련 매퍼 함수 추가

--- src/main/java/com/pser/hotel/domain/hotel/dto/mapper/HotelMapper.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/mapper/HotelMapper.java
@@ -1,8 +1,15 @@
 package com.pser.hotel.domain.hotel.dto.mapper;
 
+import com.pser.hotel.domain.hotel.domain.Amenity;
 import com.pser.hotel.domain.hotel.domain.Facility;
 import com.pser.hotel.domain.hotel.domain.Hotel;
 import com.pser.hotel.domain.hotel.domain.HotelImage;
+import com.pser.hotel.domain.hotel.domain.Room;
+import com.pser.hotel.domain.hotel.domain.RoomImage;
+import com.pser.hotel.domain.hotel.dto.AmenityDto;
+import com.pser.hotel.domain.hotel.dto.FacilityDto;
+import com.pser.hotel.domain.hotel.dto.HotelDto;
+import com.pser.hotel.domain.hotel.dto.RoomDto;
 import com.pser.hotel.domain.hotel.dto.request.HotelCreateRequest;
 import com.pser.hotel.domain.hotel.dto.request.HotelUpdateRequest;
 import com.pser.hotel.domain.hotel.dto.response.HotelResponse;
@@ -14,11 +21,22 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface HotelMapper {
+    @Named("getImageUrls")
+    default List<String> getImageUrls(List<HotelImage> hotelImages) {
+        return hotelImages.stream().map(HotelImage::getImageUrl).toList();
+    }
+
+    @Named("getRoomImageUrls")
+    default List<String> getRoomImageUrls(List<RoomImage> roomImages) {
+        return roomImages.stream().map(RoomImage::getImageUrl).toList();
+    }
+
     default HotelResponse changeToHotelResponse(Hotel hotel, double averageRating, int salePrice, int previousPrice) {
         HotelResponse hotelResponse = changeToHotelResponseExcludeImage(hotel);
         List<String> imageUrls = hotel.getImages().stream()
@@ -73,4 +91,18 @@ public interface HotelMapper {
     HotelResponse changeToHotelResponseExcludeImage(Hotel hotel);
 
     HotelSummaryResponse changeToHotelSummaryResponseExcludeImage(Hotel hotel);
+
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "images", source = "images", qualifiedByName = "getImageUrls")
+    HotelDto toDto(Hotel hotel);
+
+    @Mapping(target = "hotelId", source = "hotel.id")
+    FacilityDto toDto(Facility facility);
+
+    @Mapping(target = "hotelId", source = "hotel.id")
+    @Mapping(target = "roomImages", source = "roomImages", qualifiedByName = "getRoomImageUrls")
+    RoomDto toDto(Room room);
+
+    @Mapping(target = "roomId", source = "room.id")
+    AmenityDto toDto(Amenity amenity);
 }
