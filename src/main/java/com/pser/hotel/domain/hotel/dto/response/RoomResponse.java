ae810526d5616055f9d54491681a34aee52ed826
wooyong99 <wulovesk@naver.com>
2024-06-07 20:34:34 +0900

    refactor : RoomResponse 수정

--- src/main/java/com/pser/hotel/domain/hotel/dto/response/RoomResponse.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/response/RoomResponse.java
@@ -3,24 +3,64 @@ package com.pser.hotel.domain.hotel.dto.response;
 import com.pser.hotel.domain.hotel.domain.Amenity;
 import com.querydsl.core.annotations.QueryProjection;
 import java.time.LocalTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RoomResponse {
+
+    private Long id;
+
     private String name;
+
     private String description;
+
     private String precaution;
+
     private int price;
+
     private LocalTime checkIn;
+
     private LocalTime checkOut;
+
     private int standardCapacity;
+
     private int maxCapacity;
+
     private int totalRooms;
-    private Amenity amenity;
+
+    private Boolean heatingSystem = false;
+
+    private Boolean tv = false;
+
+    private Boolean refrigerator = false;
+
+    private Boolean airConditioner = false;
+
+    private Boolean washer = false;
+
+    private Boolean terrace = false;
+
+    private Boolean coffeeMachine = false;
+
+    private Boolean internet = false;
+
+    private Boolean kitchen = false;
+
+    private Boolean bathtub = false;
+
+    private Boolean iron = false;
+
+    private Boolean pool = false;
+
+    private Boolean pet = false;
+
+    private Boolean inAnnex = false;
 
     @QueryProjection
     public RoomResponse(String name, String description, String precaution, int price, LocalTime checkIn,
@@ -34,6 +74,19 @@ public class RoomResponse {
         this.standardCapacity = standardCapacity;
         this.maxCapacity = maxCapacity;
         this.totalRooms = totalRooms;
-        this.amenity = amenity;
+        this.heatingSystem = amenity.getHeatingSystem();
+        this.tv = amenity.getTv();
+        this.refrigerator = amenity.getRefrigerator();
+        this.airConditioner = amenity.getAirConditioner();
+        this.washer = amenity.getWasher();
+        this.terrace = amenity.getTerrace();
+        this.coffeeMachine = amenity.getCoffeeMachine();
+        this.internet = amenity.getInternet();
+        this.kitchen = amenity.getKitchen();
+        this.bathtub = amenity.getBathtub();
+        this.iron = amenity.getIron();
+        this.pool = amenity.getPool();
+        this.pet = amenity.getPet();
+        this.inAnnex = amenity.getInAnnex();
     }
 }
