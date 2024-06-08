07bd2a9d3270884d74dda5e5596e575d31e9de03
amanecopse <amanecopse@gmail.com>
2024-06-08 12:05:02 +0900

    [KSP-275] fix: 객실 DTO 매퍼 누락 수정

--- src/main/java/com/pser/hotel/domain/hotel/dto/mapper/RoomMapper.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/mapper/RoomMapper.java
@@ -2,19 +2,27 @@ package com.pser.hotel.domain.hotel.dto.mapper;
 
 import com.pser.hotel.domain.hotel.domain.Amenity;
 import com.pser.hotel.domain.hotel.domain.Room;
+import com.pser.hotel.domain.hotel.domain.RoomImage;
 import com.pser.hotel.domain.hotel.dto.AmenityDto;
 import com.pser.hotel.domain.hotel.dto.RoomDto;
 import com.pser.hotel.domain.hotel.dto.request.RoomRequest;
+import java.util.List;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface RoomMapper {
+    @Named("getRoomImageUrls")
+    default List<String> getRoomImageUrls(List<RoomImage> roomImages) {
+        return roomImages.stream().map(RoomImage::getImageUrl).toList();
+    }
+
     @Mapping(target = "hotelId", source = "hotel.id")
     @Mapping(target = "roomImages", source = "roomImages", qualifiedByName = "getRoomImageUrls")
     RoomDto toDto(Room room);
