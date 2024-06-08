793900f178af18503920ce6256e3cf0955e12dde
amanecopse <amanecopse@gmail.com>
2024-06-08 11:20:54 +0900

    [KSP-275] feat: 객실 관련 DTO 매퍼 함수 추가

--- src/main/java/com/pser/hotel/domain/hotel/dto/mapper/RoomMapper.java
+++ src/main/java/com/pser/hotel/domain/hotel/dto/mapper/RoomMapper.java
@@ -1,9 +1,13 @@
 package com.pser.hotel.domain.hotel.dto.mapper;
 
+import com.pser.hotel.domain.hotel.domain.Amenity;
 import com.pser.hotel.domain.hotel.domain.Room;
+import com.pser.hotel.domain.hotel.dto.AmenityDto;
+import com.pser.hotel.domain.hotel.dto.RoomDto;
 import com.pser.hotel.domain.hotel.dto.request.RoomRequest;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValueCheckStrategy;
@@ -11,6 +15,12 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface RoomMapper {
+    @Mapping(target = "hotelId", source = "hotel.id")
+    @Mapping(target = "roomImages", source = "roomImages", qualifiedByName = "getRoomImageUrls")
+    RoomDto toDto(Room room);
+
+    @Mapping(target = "roomId", source = "room.id")
+    AmenityDto toDto(Amenity amenity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
     void updateRoomFromDto(RoomRequest request, @MappingTarget Room room);
